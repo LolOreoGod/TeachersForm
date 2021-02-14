9d9ab7f6d5d78e80799218cf045482c3cf061a0a
alizhang21 <75001461+alizhang21@users.noreply.github.com>
2021-02-14 11:30:17 -0800

    Update StudentForm.vue

--- src/components/StudentForm.vue
+++ src/components/StudentForm.vue
@@ -41,28 +41,19 @@
       </div>
 
       <!--trash button-->
-      <div class="trash_can">
-        <div class="trash_symbol" @click="deleteOptionSet(index)">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-trash"
-            viewBox="0 0 16 16"
+      <button
+            id="deleting_mc_button"
+            class="deleting_mc_button"
+            @click="deleteOptionSet(index, optionsetindex)"
           >
-            <path
-              d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-            />
-          </svg>
-        </div>
-      </div>
+            -
+      </button>
     </div>
-    <button @click="addNewOptionSet">Add</button>
+    <button 
+        id="adding_mc_button"
+        class="adding_mc_button"
+        @click="addNewOptionSet">+
+    </button>
     <div class="submit-button">Submit</div>
   </div>
   <!--form-->
@@ -200,7 +191,7 @@ export default {
 
 /* option */
 #optionSet {
-  width: 600px;
+  width: 90%;
   height: auto;
   background-color: #eeeeeeff;
   margin-left: auto;
@@ -223,7 +214,7 @@ export default {
 }
 /*question and answer text inputs in choice question box*/
 .topic-choice-text {
-  width: 550px;
+  width: 90%;
   height: 30px;
   margin-left: auto;
   margin-right: auto;
@@ -232,7 +223,7 @@ export default {
 }
 
 .topic-choice-answer {
-  width: 550px;
+  width: 90%;
   height: 80px;
   margin-left: auto;
   margin-right: auto;
@@ -251,5 +242,32 @@ export default {
   color: white;
   display: block;
   text-align: center;
+}
+
+.deleting_mc_button {
+  float: center;
+  margin-top: 1%;
+  margin-bottom: 1%;
+  width: 50px;
+  font-size: 15px
+}
+
+/* adding multiple choice/checkbox button */
+.adding_mc_button {
+  background-color: white;
+  margin-bottom: 1%;
+  border: none;
+  color: grey;
+  border: 1px solid black;
+  width: 100px;
+  height: 30px;
+  font-size: 20px;
+  text-decoration: bold;
+  border-radius: 10%;
+}
+
+.adding_mc_button:hover {
+  background-color: #779fa1ff;
+  color: white;
 }
 </style>
